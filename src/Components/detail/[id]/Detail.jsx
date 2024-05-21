3e73ccffd2d03f43dab171927d3bb142dff91687
raffyalbar30 <geminglol09@gmail.com>
2024-05-21 12:21:08 +0700

    trailer fitur video

--- src/Components/detail/[id]/Detail.jsx
+++ src/Components/detail/[id]/Detail.jsx
@@ -3,12 +3,14 @@ import { Raffnimeapi } from '../../../libs/Api';
 import { useParams } from 'react-router-dom';
 import Footer from '../../Footer/footer';
 import { CiPlay1 } from "react-icons/ci";
-
+import { AiTwotoneCloseCircle } from "react-icons/ai";
 
 
 const Detail = () => {
+   
     let { id } = useParams();
     const [getDetail, setgetDetail] = useState([]);
+    const [vidioplayer, setvidioplayer] = useState(true);
     const detaildata =  async () => {
         try {
             const Response = await Raffnimeapi(`anime/${id}`);
@@ -21,22 +23,32 @@ const Detail = () => {
     useEffect(()=> {
         detaildata();
     }, []);
+
+    const Handlevidioplayer = () => {
+        setvidioplayer(true);
+    }
+
+    const Handleclosevidioplayer = () => {
+        setvidioplayer(false);
+    }
+
+
     return (
         <>
          <div className='w-full absolute top-0  -z-10'>
          {/* <video className="w-full -z-10" loop>
-              <source src="" type="video/mp4"  className='h-12'/>
+              
          </video> */}
         <div className='mt-24 ml-4'>
-            <div className='mt-4'>
-                 <img src={getDetail.images.jpg.image_url} alt={getDetail.images.jpg.image_url} className='rounded-lg'/> 
-            </div>
+             {/* <div className='mt-4'>
+                  <img src={getDetail.images.jpg.image_url} alt={getDetail.images.jpg.image_url} className='rounded-lg'/>  
+            </div>  */}
             <div className='pb-4 pt-6'>
               <p className='text-2xl font-semibold'>{getDetail.title} / {getDetail.title_japanese}</p>
               <p>{getDetail.year} | {getDetail.rating}</p>  
               <p>{ getDetail.episodes} / { getDetail.duration}</p>
                <div className='pt-2 '>
-                   <button className='px-8 py-2 border rounded-lg flex items-center gap-x-2 hover:border-blue-700'>
+                   <button className='px-8 py-2 border rounded-lg flex items-center gap-x-2 hover:border-blue-700' onClick={Handlevidioplayer}>
                     { <CiPlay1 className='text-lg' />}
                       View Trailer
                     </button>
@@ -46,7 +58,12 @@ const Detail = () => {
                <p className='text-xl'>{getDetail.synopsis}</p>
             </div>  
         </div> 
-          
+          <div className={`fixed bottom-2 right-2 ${vidioplayer ? 'active' : 'hidden'}`}>
+            <AiTwotoneCloseCircle onClick={Handleclosevidioplayer} />
+            <iframe width="420" height="315"
+               src={getDetail.trailer.url}>
+            </iframe>  
+         </div>  
        <Footer></Footer>
         </div>
 
