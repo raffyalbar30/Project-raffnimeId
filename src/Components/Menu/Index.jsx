f7a70a213fee46eb2b8cd19b649f66ccf16c520c
raffyalbar30 <geminglol09@gmail.com>
2024-05-29 20:19:10 +0700

    update meta data title

--- src/Components/Menu/Index.jsx
+++ src/Components/Menu/Index.jsx
@@ -2,8 +2,35 @@ import React from 'react';
 import Nav from '../Navbar/Nav'; 
 import Menu from '../Menu/Menu';
 import SerchAnime from '../SecrhAnime/SerchAnime';
+import { useRef } from 'react';
+import { useEffect } from 'react';
+import Typed from "typed.js";
+
 
 const Index= () => {
+    const type = useRef();
+
+    useEffect(() => {
+        var typed = new Typed( type.current, {
+            strings: [
+                'Let see my recomended anime !!🚀🤔',
+                'cek my recomended👇'
+              ],
+              typeSpeed: 100,
+              loop: true,
+              showCursor: true,
+              cursorChar: '|',
+              startDelay: 2000,
+              fadeOut: true,
+      
+          });
+          return () => {
+            typed.destroy();
+          };
+      
+    }, []);
+
+    
     return (
         <>
         <div id="navbar" className='bg-transparent bg-opacity-80 backdrop-saturate-0 w-full h-20 -z-30 flex justify-between'>
@@ -14,8 +41,8 @@ const Index= () => {
               <Menu></Menu>
         </div>
         <div className='-z-20'>
-            <p className='text-center md:mt-60 mt-2 md:text-5xl font-medium'>Nikonikoni Let's view Raffnime.id</p>
-            <p className='text-center md:text-5xl font-medium'>My recomended you can See top recomendedlist</p>
+            <p className='text-center md:mt-60 mt-2 md:text-5xl font-medium'> Hello !! Need help ? </p>
+            <p className='text-center  md:text-5xl font-medium' ref={type}></p>
         <div className='flex justify-center md:mt-8 mt-4'>
             <button className='bg-transparent md:px-28 md:p-4 px-12 p-2 rounded-xl border hover:border-slate-500'>
                 check my list
